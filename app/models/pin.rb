5d8b38070655bfbdb3b14854e907e443322000f7
aaronwbrown <aaron.w.brown13@gmail.com>
2013-08-15 17:54:21 -0700

    Removing redundant classes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,14 +5,13 @@ attr_accessible :show
 attr_accessible :edit
 attr_accessible :image
 
-class Pin < ActiveRecord::Base
-  validates :description, presence: true
-  validates :user_id, presence: true
-  has_attached_file :image, styles: { medium: "320x240>"}
-  validates_attachment :image, presence: true,
-	                      content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-	                      size: { less_than: 5.megabytes }
-  belongs_to :user
+validates :description, presence: true
+validates :user_id, presence: true
+has_attached_file :image, styles: { medium: "320x240>"}
+validates_attachment :image, presence: true,
+                      content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                      size: { less_than: 5.megabytes }
+belongs_to :user
 
 def pin_params
  params.require(:pin).permit(:description, :image)
