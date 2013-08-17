a5f6da4bb6400dcebb76d70751b04caeb5b8af01
aaronwbrown <aaron.w.brown13@gmail.com>
2013-08-16 17:37:02 -0700

    Paperclip Upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,18 +1,19 @@
 class Pin < ActiveRecord::Base
-attr_accessible :description
-attr_accessible :destroy
-attr_accessible :show
-attr_accessible :edit
-attr_accessible :image
+	attr_accessible :description
+	attr_accessible :destroy
+	attr_accessible :show
+	attr_accessible :edit
+	attr_accessible :image
 
-validates :description, presence: true
-validates :user_id, presence: true
-has_attached_file :image, styles: { medium: "320x240>"}
-validates_attachment :image, presence: true,
-                      content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-                      size: { less_than: 5.megabytes }
-belongs_to :user
+	validates :description, presence: true
+	validates :user_id, presence: true
+	has_attached_file :image, styles: { medium: "320x240>"}
+	validates_attachment :image, presence: true,
+	                      content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+	                      size: { less_than: 5.megabytes }
+	belongs_to :user
 
-def pin_params
- params.require(:pin).permit(:description, :image)
+	def pin_params
+	 params.require(:pin).permit(:description, :image)
+	end
 end
