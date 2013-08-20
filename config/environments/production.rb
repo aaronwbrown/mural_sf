518778bb116ef2f64568e0b634142932b0e434a2
aaronwbrown <aaron.w.brown13@gmail.com>
2013-08-19 22:43:31 -0700

    Heroku S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,11 @@ MuralSf::Application.configure do
 
 #configuring Amazon S3 for Paperclip file uploads
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['mural_sf'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 end
